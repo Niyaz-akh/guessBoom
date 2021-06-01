8edbff76628df4cd9c98aece86986d4de3f43931
Niyaz_Akh <niyaz.akh@ya.ru>
2021-06-02 00:24:31 +0500

    refactor BoomServiceImpl

--- src/main/java/akh/niyaz/BoomServiceImpl.java
+++ src/main/java/akh/niyaz/BoomServiceImpl.java
@@ -16,9 +16,9 @@ public class BoomServiceImpl implements BoomService {
     public double getOverpressureValue(Boom boom, double distanceToCenterExplosion) {
         final double forecastRadius = getForecastRadius(boom, distanceToCenterExplosion);
         if (forecastRadius <= 6.2)
-            return overpressureValueForSmallForecastRadius(boom, forecastRadius);
+            return overpressureValueForSmallForecastRadius(forecastRadius);
         else
-            return overpressureValueForBigForecastRadius(boom, forecastRadius);
+            return overpressureValueForBigForecastRadius(forecastRadius);
     }
 
     private double getForecastRadius(Boom boom, double distanceToCenterExplosion) {
@@ -35,14 +35,14 @@ public class BoomServiceImpl implements BoomService {
         return Math.pow(2 * n * q * k, 1.0 / 3);
     }
 
-    private double overpressureValueForSmallForecastRadius(Boom boom, double forecastRadius) {
+    private double overpressureValueForSmallForecastRadius(double forecastRadius) {
         MathContext mathContext = new MathContext(5, RoundingMode.HALF_UP);
         double result = 700 / (3 * (Math.sqrt(1 + Math.pow(forecastRadius, 3)) - 1));
         BigDecimal overpressure = new BigDecimal(result, mathContext);
         return overpressure.doubleValue();
     }
 
-    private double overpressureValueForBigForecastRadius(Boom boom, double forecastRadius) {
+    private double overpressureValueForBigForecastRadius(double forecastRadius) {
         MathContext mathContext = new MathContext(5, RoundingMode.HALF_UP);
         double result = 70 / (forecastRadius * Math.sqrt(Math.log(forecastRadius) - 0.332));
         BigDecimal overpressure = new BigDecimal(result, mathContext);
